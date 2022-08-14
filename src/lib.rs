2a52e0b1b742e56b4f985f8a236ec1f48b12aab1
amigin <amigin@gmail.com>
2022-08-14 20:50:42 +0300

    Added Logger

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ mod log_event;
 mod logger;
 mod logger_reader;
 
+use std::sync::Arc;
+
 pub use log_event::{LogLevel, MyLogEvent};
 pub use logger::MyLogger;
 pub use logger_reader::MyLoggerReader;
 
 lazy_static::lazy_static! {
-    pub static ref LOGGER: MyLogger = {
-        MyLogger::new()
+    pub static ref LOGGER: Arc<MyLogger> = {
+        Arc::new(MyLogger::new())
     };
 }
